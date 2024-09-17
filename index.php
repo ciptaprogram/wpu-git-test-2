6ab04b534017be9774e11ba2d82555801d6ab9c1
webprogrammingunpas <arhidajat@gmail.com>
2024-09-17 14:38:57 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem omnis laboriosam animi accusamus tempore repudiandae quasi ducimus similique magni, nostrum atque nesciunt quia nemo ipsum quos maxime neque laborum. Possimus.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
